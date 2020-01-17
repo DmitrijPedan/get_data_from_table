65151817f3270191a3ed243d415a8ec8ecab2866
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-01-17 09:19:02 +0200

    getted Object

--- js/app.js
+++ js/app.js
@@ -3,35 +3,27 @@ let x = document.querySelector('body table tbody tr:nth-child(2) td:nth-child(2)
 // console.log(x);
 
 
-// массив значений шапки таблицы (ключи для обьектов) возвращает массив
-function getKeysFromTable () {
-    let res = [...document.querySelectorAll('table thead th')].map(el => el.innerHTML.replace(/\s+/g, '').toLowerCase()); 
-    // console.log(res);
-    return res;
-}
-
-
-
-function getRowFromTable (n) {
-    let res = [...document.querySelectorAll(`body table tbody tr:nth-child(${n}) td`)].map(el => el.innerHTML); 
-    // console.log(res);
-    return res;
-}
-
 function getObjectFromTable () {
-let tkeys = getKeysFromTable();
-let tvalues = getRowFromTable(1);
-
-let res = {};
-for (let i = 0; i < tkeys.length; i++) {
-    res[tkeys[i]] = tvalues[i]
-
+    let count = [...document.querySelectorAll(`body table tbody tr`)].length - 2; 
+    let keys = [...document.querySelectorAll('table thead th')].map(el => el.innerHTML.replace(/\s+/g, '').toLowerCase()); 
+    // let row = [...document.querySelectorAll(`body table tbody tr:nth-child(${n}) td`)].map(el => el.innerHTML); 
+    let res = [];
+    
+    for (i = 0; i < count; i++) {
+        res[i] = {}
+        let row = [...document.querySelectorAll(`body table tbody tr:nth-child(${i+1}) td`)].map(el => el.innerHTML); 
+        for (let j = 0; j < keys.length; j++) {
+            res[i][keys[j]] = row[j]
+        }
+    }
+    console.log(res);
+    return res;
 }
- console.log(res);
 
-}
+getObjectFromTable()
 
-getObjectFromTable ();
+
+
 
 
 
